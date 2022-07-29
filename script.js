ba893c518bd4bd8ca662911cc34183aa28e1662f
Paul1565 <pavel.ryzhkov.99@mail.ru>
2022-07-29 15:58:46 +0300

    SearchingSymbols

---
+++ script.js
@@ -0,0 +1,16 @@
+//alert( window.innerWidth );
+// noinspection JSUnusedGlobalSymbols,JSUnresolvedFunction
+
+let symb = "";
+let str = "";
+function stringLengthDefinition() {
+    let symbArr = [];
+    for (index; index < str.length; index++) {
+        if (indexof(symb) != -1) {
+            symbArr.push(symb);
+        } else {
+            console.log('Next')
+        }
+    }
+    return symbArr.length;
+}
